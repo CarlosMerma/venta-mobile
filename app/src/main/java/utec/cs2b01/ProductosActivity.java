c9ab3b59918d203bb1cd41cb5351fea5811cd42e
Guillermo Sanchez <guillermo.sanchez@utec.edu.pe>
2020-07-22 13:54:07 -0500

    mobile mejorado
    mobilemejorado

---
+++ app/src/main/java/utec/cs2b01/ProductosActivity.java
@@ -0,0 +1,17 @@
+package utec.cs2b01;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class ProductosActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+
+    public Activity getActivity(){
+        return this;
+    }
+
+
+}
